fe689cd9170b18b54585e1a82ce5a31b728c9406
John DeFrain <defrain.3@gmail.com>
2019-05-14 20:10:17 -0400

    setup spring boot

---
+++ src/main/java/com/defrainPhoto/pictime/PicTimeApplication.java
@@ -0,0 +1,13 @@
+package com.defrainPhoto.pictime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicTimeApplication.class, args);
+	}
+
+}
