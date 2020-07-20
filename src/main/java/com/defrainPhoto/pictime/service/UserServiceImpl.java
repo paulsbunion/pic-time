47c0e60c0347c2f6054b43e4524b5de964a1dcbb
John DeFrain <defrain.3@gmail.com>
2020-07-20 19:53:10 -0400

    Moved project local to eclipse 2020

--- src/main/java/com/defrainPhoto/pictime/service/UserServiceImpl.java
+++ src/main/java/com/defrainPhoto/pictime/service/UserServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
