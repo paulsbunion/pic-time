de1b303cd2ea06cf268f305d2871812d2e1c2e1d
John DeFrain <defrain.3@gmail.com>
2020-08-04 21:01:59 -0400

    added try/catch block for repeat delete

--- src/main/java/com/defrainPhoto/pictime/controller/ClientMVCController.java
+++ src/main/java/com/defrainPhoto/pictime/controller/ClientMVCController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -79,7 +80,12 @@ public class ClientMVCController {
 	
 	@GetMapping("delete/{id}")
 	public String deleteClient(@PathVariable("id") long id) {
-		clientController.delete(id);
+		try {
+			clientController.delete(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			// TODO: handle exception
+		}
 		return "redirect:" + MVC_CLIENT_URL_BASE + "list";
 	}
 	/**
