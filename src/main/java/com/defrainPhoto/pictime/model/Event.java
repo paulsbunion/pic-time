4b4dc7d3244e59abfbd644b7e997c8dc7327a962
John DeFrain <defrain.3@gmail.com>
2020-09-03 19:05:27 -0400

    Added LocalDate to Event

--- src/main/java/com/defrainPhoto/pictime/model/Event.java
+++ src/main/java/com/defrainPhoto/pictime/model/Event.java
@@ -1,5 +1,6 @@
 package com.defrainPhoto.pictime.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +23,17 @@ public class Event {
 	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
 	@GenericGenerator(name = "native", strategy = "native")
 	private long id;
+
 	private String eventName;
-	
+
+	private LocalDate date;
+
 	@ManyToOne
 	private EventType eventType;
-	
-//	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY, mappedBy = "event")
-//	private Timeline timeline;
+
 	@OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Timeslot> timeslots = new ArrayList<Timeslot>();
-	
+
 	private String extraCost;
 	private String notes;
 	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY, mappedBy = "event")
@@ -41,24 +43,38 @@ public class Event {
 //	private Set<User> users;
 	@OneToMany(mappedBy = "eventUserId", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<EventUser> eventUsers = new ArrayList<EventUser>();
-	
-	public Event() {}
-	
+
+	public Event() {
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getEventName() {
 		return eventName;
 	}
+
 	public void setEventName(String eventName) {
 		this.eventName = eventName;
 	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
 	public EventType getEventType() {
 		return eventType;
 	}
+
 	public void setEventType(EventType eventType) {
 		this.eventType = eventType;
 	}
@@ -70,7 +86,7 @@ public class Event {
 	public void setTimeslots(List<Timeslot> timeslots) {
 		this.timeslots = timeslots;
 	}
-	
+
 	public void addTimeslot(Timeslot timeslot) {
 		this.timeslots.add(timeslot);
 		timeslot.setEvent(this);
@@ -79,31 +95,44 @@ public class Event {
 	public String getExtraCost() {
 		return extraCost;
 	}
+
 	public void setExtraCost(String extraCost) {
 		this.extraCost = extraCost;
 	}
+
 	public String getNotes() {
 		return notes;
 	}
+
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
+
 	public Mileage getMileage() {
 		return mileage;
 	}
+
 	public void setMileage(Mileage mileage) {
 		this.mileage = mileage;
 	}
-		
+
 	public void addEventUser(EventUser eventUser) {
 		this.eventUsers.add(eventUser);
 		eventUser.setEvent(this);
+	}
+	
+
+	public List<EventUser> getEventUsers() {
+		return eventUsers;
+	}
+
+	public void setEventUsers(List<EventUser> eventUsers) {
+		this.eventUsers = eventUsers;
 	}
 
 	@Override
 	public String toString() {
 		return "eventId=" + id;
 	}
-	
-	
+
 }
