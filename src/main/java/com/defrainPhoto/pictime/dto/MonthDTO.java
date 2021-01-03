9414f7c7904d9af6f02ec094e19cf132e336c351
John DeFrain <defrain.3@gmail.com>
2021-01-03 15:14:26 -0500

    refactored monthDTO

--- src/main/java/com/defrainPhoto/pictime/dto/MonthDTO.java
+++ src/main/java/com/defrainPhoto/pictime/dto/MonthDTO.java
@@ -1,12 +1,12 @@
 package com.defrainPhoto.pictime.dto;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
 public class MonthDTO {
 
-	private int[] daysInEveryMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private int daysInMonth;
 	private int weekStartDay;
 	private String monthName;
@@ -15,12 +15,17 @@ public class MonthDTO {
 	private int day;
 	private String dayName;
 
-	private int previousMonthLink;
-	private int nextMonthLink;
-	private int previousYearLink;
-	private int nextYearLink;
-	private int previousDay;
-	private int nextDay;
+	private LocalDate previousDay;
+	private LocalDate previousMonth;
+	private LocalDate previousYear;
+	
+	private LocalDate nextDay;
+	private LocalDate nextMonth;
+	private LocalDate nextYear;
+	
+	private DateTimeFormatter monthformatter = DateTimeFormatter.ofPattern("YYYY/M");
+	private DateTimeFormatter dayformatter = DateTimeFormatter.ofPattern("YYYY/M/d");
+	
 	
 	public MonthDTO() {
 	}
@@ -31,38 +36,22 @@ public class MonthDTO {
 
 	public MonthDTO(Integer year, Integer month, Integer day) {
 		LocalDate date = LocalDate.of(year, month, day);
-		daysInMonth = daysInEveryMonth[month - 1];
+		
+		previousDay = date.minusDays(1);
+		previousMonth = date.minusMonths(1);
+		previousYear = date.minusYears(1);
+		
+		nextDay = date.plusDays(1);
+		nextMonth= date.plusMonths(1);
+		nextYear = date.plusYears(1);
+		
+		daysInMonth = date.lengthOfMonth();
 		weekStartDay = date.getDayOfWeek().getValue();
 		monthName = date.getMonth().getDisplayName(TextStyle.FULL, Locale.US);
-//		monthName = date.getMonth().toString();
 		this.year = date.getYear();
 		this.month = date.getMonth().getValue();
 		this.day = date.getDayOfMonth();
 		this.dayName = date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US);
-//		this.dayName = date.getDayOfWeek().toString();
-
-		int tempYear = year;
-		int tempMonth = month;
-
-		if (month == 1) {
-			tempYear = year - 1;
-			tempMonth = 12;
-		} else {
-			tempYear = year;
-			tempMonth = month - 1;
-		}
-		previousYearLink = tempYear;
-		previousMonthLink = tempMonth;
-
-		if (month == 12) {
-			tempYear = year + 1;
-			tempMonth = 1;
-		} else {
-			tempYear = year;
-			tempMonth = month + 1;
-		}
-		nextYearLink = tempYear;
-		nextMonthLink = tempMonth;
 	}
 
 	public int getDaysInMonth() {
@@ -121,52 +110,19 @@ public class MonthDTO {
 		this.dayName = dayName;
 	}
 
-	public int getPreviousMonthLink() {
-		return previousMonthLink;
+	public String getPreviousDayLink() {
+		return  previousDay.format(dayformatter);
 	}
-
-	public void setPreviousMonthLink(int previousMonthLink) {
-		this.previousMonthLink = previousMonthLink;
+	
+	public String getNextDayLink() {
+		return  nextDay.format(dayformatter);
 	}
-
-	public int getNextMonthLink() {
-		return nextMonthLink;
+	
+	public String getPreviousMonthLink() {
+		return  previousMonth.format(monthformatter);
 	}
-
-	public void setNextMonthLink(int nextMonthLink) {
-		this.nextMonthLink = nextMonthLink;
+	
+	public String getNextMonthLink() {
+		return  nextMonth.format(monthformatter);
 	}
-
-	public int getPreviousYearLink() {
-		return previousYearLink;
-	}
-
-	public void setPreviousYearLink(int previousYearLink) {
-		this.previousYearLink = previousYearLink;
-	}
-
-	public int getNextYearLink() {
-		return nextYearLink;
-	}
-
-	public void setNextYearLink(int nextYearLink) {
-		this.nextYearLink = nextYearLink;
-	}
-
-	public int getPreviousDay() {
-		return previousDay;
-	}
-
-	public void setPreviousDay(int previousDay) {
-		this.previousDay = previousDay;
-	}
-
-	public int getNextDay() {
-		return nextDay;
-	}
-
-	public void setNextDay(int nextDay) {
-		this.nextDay = nextDay;
-	}
-
 }
